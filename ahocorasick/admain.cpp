ebe848391f773223c5faa529d13db0c092bc60bf
yuki7tsk <n231051y@mail.kyutech.jp>
2017-11-21 14:02:59 +0900

    init

---
+++ ahocorasick/admain.cpp
@@ -0,0 +1,180 @@
+//============================================================================
+//admain.cpp
+//============================================================================
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <sys/time.h>
+
+using namespace std;
+
+#include "ACMachine.h"
+#include "dawg.h"
+#include "ahodawg.h"
+#include "commandargs.h"
+
+#include <time.h>
+#include <iomanip>
+#include <cstdlib>
+#include <unistd.h>
+//#include <stdlib.h>
+
+int main(const int argc, char * const * argv) {
+	ifstream ifs, addifs;
+	int init_word_num = 0;
+	int state_width = 10000;
+	int state_max = 450000;
+	bool ignore_case = false;
+
+	commandargs optargs(argc, argv, "n:f:a:imw");
+
+	pair<bool, const char*> opt;
+	opt = optargs.opt('n');
+	if (opt.first) {
+		init_word_num = atol(opt.second);
+		cout << "init_word_num = " << init_word_num << ", ";
+	}
+	opt = optargs.opt('f');
+	if (opt.first) {
+		string fname = string(opt.second);
+		cout << "fname (pattern file) = " << fname << ", ";
+		ifs.open(fname);
+	}
+	opt = optargs.opt('a');
+	if (opt.first) {
+		string addfname = string(opt.second);
+		cout << "addfname (pattern file) = " << addfname << ", ";
+		addifs.open(addfname);
+	}
+	opt = optargs.opt('i');
+	ignore_case = opt.first;
+	cout << "ignore case = " << ignore_case << ", ";
+	opt = optargs.opt('m');
+        if (opt.first) {
+		state_max = atol(opt.second);
+		cout << "state_max = " << state_max << ", ";
+	}
+	cout << endl;
+	opt = optargs.opt('w');
+        if (opt.first) {
+		state_width = atol(opt.second);
+		cout << "state_width = " << state_width << ", ";
+	}
+	cout << endl;
+
+	ahdawg ad;
+
+	vector<string> words;
+	istringstream line;
+	string tmp;
+	while (!ifs.eof()) {
+		std::getline(ifs, tmp);
+		if (tmp.empty())
+			break;
+		line.str(tmp);
+		line.clear();
+		while (!line.eof()) {
+			line >> tmp;
+			if (words.size() < init_word_num)
+				words.push_back(tmp);
+			line >> tmp;
+		}
+		if (words.size() == init_word_num)
+			break;
+	}
+	cout << "Got " << words.size() << " words: " << endl;
+
+	ad.BuildAhoandDawg(words);
+
+	ifs.close();
+	words.clear();
+	cout << "build end" << endl;
+	
+
+	//--------------------------------
+	//dynamic insertion
+	//--------------------------------
+
+
+	vector<string> addwords;
+		
+	istringstream addline;
+	string addtmp;
+		
+	int counter_max = state_max / state_width;
+		
+	int nextstatescount = 1;
+
+	vector<int> statesize_vec;
+	vector<int> wordnum_vec;
+	vector<int> time_vec;
+		
+
+	while (!addifs.eof()) {
+		std::getline(addifs, addtmp);
+		if (addtmp.empty())
+			break;
+		addline.str(addtmp);
+		addline.clear();
+		while (!addline.eof()) {
+			addline >> addtmp;
+			addwords.push_back(addtmp);
+			addline >> addtmp;
+		}
+
+	}
+	cout << "Got " << addwords.size() << " add words: " << endl;
+
+	int time = 0;
+	struct timeval start, end;
+	gettimeofday(&start, NULL);
+	for(int i = 0; i < addwords.size(); i++){
+
+		ad.DynamicBAD(addwords[i]);
+		cout << "check" << endl;
+
+		if(ad.statesize() >= (nextstatescount * state_width)){
+			gettimeofday(&end, NULL);
+			time = (end.tv_sec - start.tv_sec)*1000000 + (end.tv_usec - start.tv_usec) ;
+			time_vec.push_back(time);
+			wordnum_vec.push_back(i);
+			statesize_vec.push_back(ad.statesize());
+			cout << nextstatescount << endl;
+
+			nextstatescount++;
+		}
+
+		if(nextstatescount > counter_max)
+			break;
+	}
+	
+
+
+	for(int i = 0; i < (nextstatescount - 1); i++){
+		cout << i+1 << ",";
+		cout << statesize_vec[i] << ",";
+		cout << wordnum_vec[i] << ",";
+		cout << time_vec[i] << ",";
+		if(i == 0){
+			cout << statesize_vec[i] << ",";
+			cout << wordnum_vec[i] << ",";
+			cout << time_vec[i] << ",";
+			cout << (double)time_vec[i] / wordnum_vec[i];
+		}
+		if(i != 0){
+			cout << statesize_vec[i] - statesize_vec[i-1] << ",";
+			cout << wordnum_vec[i] - wordnum_vec[i-1] << ",";
+			cout << time_vec[i] - time_vec[i-1] << ",";
+			cout << (double)(time_vec[i]-time_vec[i-1])/(wordnum_vec[i]-wordnum_vec[i-1]);
+		}
+		cout << endl;
+	}
+
+
+	addifs.close();
+	addwords.clear();
+	
+	return 0;
+}
---
+++ ahocorasick/dawg.cpp
@@ -0,0 +1,303 @@
+//====================================
+// dawg.cpp
+//====================================
+
+#include "dawg.h"
+
+#include <deque>
+#include <algorithm>
+#include <cctype>
+
+
+DAWG::DAWG(void) {
+	setupInitialState();
+	resetState();
+}
+
+void DAWG::setupInitialState(void) {
+	transitions.clear();
+	transitions.push_back(std::map<alphabet, state_edgelabel>());
+	spointer.clear();
+	spointer.push_back(initialState());
+	torb.clear();
+	torb.push_back(true);
+	datoac.clear();
+	datoac.push_back(initialState());
+}
+
+
+int DAWG::transfer(const state current, const alphabet & c, const int pors, const bool ignore_case) {
+  std::map<alphabet, state_edgelabel>::iterator itr;
+  state newstate;
+  
+  if (ignore_case) {
+    itr = transitions[current].find(toupper(c));
+    if (itr == transitions[current].end())
+      itr = transitions[current].find(tolower(c));
+  }
+  else {
+    itr = transitions[current].find(c);
+  }
+  
+  if (itr == transitions[current].end()) {
+    return -1;
+  }
+
+  if (pors == -1) 
+	  newstate = itr->second.first;
+  else {
+	  if (pors != itr->second.second) {
+		  return -1;
+	  }
+	  newstate = itr->second.first;
+  }
+
+  return newstate;
+}
+
+
+DAWG::state DAWG::update(state sink, char c) {
+    state currentsink = sink;
+    state newsink;
+    state_edgelabel tmp;
+
+    if(transfer(currentsink, c) > 0){
+      //std::cout << "not make primary" << std::endl;
+      if(transfer(currentsink, c, true) > 0){
+		newsink = transfer(currentsink, c, true);
+		torb[newsink] = true;
+		if(datoac[newsink] == -1)
+		  dawgtoacm(true, newsink);
+		return newsink;
+	  }
+      else{
+		newsink = transfer(currentsink, c, false);
+		torb.push_back(true);
+		dawgtoacm(true);
+		return split(currentsink, newsink, c);
+	  }
+    }
+    else{
+      //std::cout << "make primary" << std::endl;
+      newsink = transitions.size();
+      tmp.first = newsink; tmp.second = true;
+      transitions.push_back(std::map<alphabet, state_edgelabel>());
+      transitions[currentsink].insert(std::make_pair(c, tmp));
+      torb.push_back(true);
+      dawgtoacm(true);
+
+      spointer.push_back(initialState());
+
+      state currentstate = currentsink;
+      int sfstate = -1;
+
+      while((currentstate != initialState()) && (sfstate == -1)){
+		currentstate = spointer[currentstate];
+		//std::cout << "current state: " << currentstate << std::endl;
+		if((transfer(currentstate, c, true) == -1) && (transfer(currentstate, c, false) == -1)){
+			//std::cout << "while: not edge" << std::endl;
+			tmp.first = newsink; tmp.second = false;
+			transitions[currentstate].insert(std::make_pair(c, tmp));
+			//std::cout << "check" << std::endl;
+		}
+		else if(transfer(currentstate, c, true) > 0){
+		  //std::cout << "while: primary" << std::endl;
+		  sfstate = transfer(currentstate, c, true);
+		}
+		else if(transfer(currentstate, c, false) > 0){
+		  //std::cout << "while: secondary" << std::endl;
+		  state child = transfer(currentstate, c, false);
+		  torb.push_back(false);
+		  dawgtoacm(false);
+		  sfstate = split(currentstate, child, c);
+		  //std::cout << "while: secondary sfstate: " << sfstate << std::endl;
+		}
+      }
+
+      if(sfstate == -1)
+		sfstate = initialState();
+
+      spointer[newsink] = sfstate;
+      return newsink;
+    }
+}
+
+DAWG::state DAWG::split(state parentstate, state childstate, char c) {
+  
+  state newchildstate;
+  std::map<alphabet, state_edgelabel>::iterator itr;
+  newchildstate = transitions.size();
+  transitions.push_back(std::map<alphabet, state_edgelabel>());
+  state_edgelabel  tmp;
+  tmp.first = newchildstate; tmp.second = true;
+  
+  itr = transitions[parentstate].find(c);
+  transitions[parentstate].erase(itr);
+  transitions[parentstate].insert(std::make_pair(c, tmp));
+  transitions[newchildstate].insert(transitions[childstate].begin(), transitions[childstate].end());
+  for(auto & assoc:transitions[newchildstate]) 
+    assoc.second.second = false;
+
+  spointer.push_back(spointer[childstate]);
+  spointer[childstate] = newchildstate;
+
+  state currentstate;
+  currentstate = parentstate;
+  tmp.first = newchildstate; tmp.second = false;
+
+  while(currentstate != initialState()){
+    currentstate = spointer[currentstate];
+    itr = transitions[currentstate].find(c);
+    if(itr->second.second == false){
+      transitions[currentstate].erase(itr);
+      transitions[currentstate].insert(std::make_pair(c, tmp));
+      
+    }
+    else{
+      break;
+    }
+    
+  }
+
+  return newchildstate;
+  
+}
+
+
+template <typename T>
+std::vector<DAWG::fstates> DAWG::getFailStates(int position, const T patt[]) {
+  const std::string p(patt);
+  return getFailStates(position, p);
+}
+
+//std::vector<DAWG::fstates> DAWG::getFailStates(int position, std::string patt) {
+template <typename T>
+std::vector<DAWG::fstates> DAWG::getFailStates(int position, const T & patt) {
+  std::vector<fstates> failstates;
+  state activestate = initialState();
+  //state cstate;
+  state currentstate;
+  state pos;
+  state state_tmp;
+  fstates f_tmp;
+  int pattsize = patt.length();
+  std::deque<state> st;
+  std::deque<int> st_pos;
+
+  for (int i = 0; i <= pattsize; i++) {
+    if (i > position) {
+      st.push_front(activestate);
+      st_pos.push_front(i);
+    }
+
+
+    if ((transfer(activestate, patt[i]) == -1))
+      break;
+
+    state_tmp = transfer(activestate, patt[i]);
+    
+    activestate = state_tmp;
+  }
+
+  std::deque<state> queue;
+  std::deque<int> queue_pos;
+  std::vector<state> mark;
+
+
+  while (!st.empty()) {
+    state_tmp = st.front();
+    pos = st_pos.front();
+    st.pop_front();
+    st_pos.pop_front();
+    queue.push_back(state_tmp);
+
+    while (!queue.empty()) {
+      currentstate = queue.front();
+      queue.pop_front();
+
+	  auto itr = std::find(mark.begin(), mark.end(), currentstate);
+	  if (itr == mark.end()) {
+		mark.push_back(currentstate);
+		if ((torb[currentstate] == true)) {
+			f_tmp.first = datoac[currentstate];
+			f_tmp.second = pos;
+			failstates.push_back(f_tmp);
+	    }
+	    else {
+	      /*
+			for (state s = 0; s < spointer.size(); s++) {
+				if(spointer[s] == currentstate)
+					queue.push_back(s);
+			}
+	      */
+	      for(auto inf : inv_sp[currentstate]){
+		queue.push_back(inf);
+	      }
+	    }
+	  }
+
+    }
+
+  }
+  return failstates;
+}
+
+
+template <typename T>
+std::vector<DAWG::state> DAWG::getOutStates(const T patt[]) {
+  const std::string p(patt);
+  return getOutStates(p);
+}
+
+
+//std::vector<DAWG::state> DAWG::getOutStates(std::string patt) {
+template <typename T>
+std::vector<DAWG::state> DAWG::getOutStates(const T & patt) {
+  std::vector<state> outstates;
+  state activestate = initialState();
+  state cstate;
+  state currentstate;
+  //state tonum;
+  int pos;
+  int pattsize = patt.length();
+  std::deque<state> st;
+
+  for (int i = 0; i < pattsize; i++) {
+    
+    pos = transfer(activestate, patt[i]);
+
+    if (pos == -1)
+      break;
+    
+    activestate = pos;
+  }
+
+
+  if (pos == -1)
+	  return outstates;
+  
+  std::deque<state> queue;
+  queue.push_back(activestate);
+
+  while (!queue.empty()) {
+
+    cstate = queue.front();
+    queue.pop_front();
+    if(torb[cstate] == true)
+      outstates.push_back(datoac[cstate]);
+    
+    /*
+    for (state s = 0; s < spointer.size(); s++) {
+      if(spointer[s] == cstate)
+		queue.push_back(s);
+    }
+    */
+    for(auto inf : inv_sp[cstate]){
+      queue.push_back(inf);
+    }
+
+  }
+  return outstates;
+}
+
+
